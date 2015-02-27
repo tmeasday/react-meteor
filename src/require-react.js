2c63fb20a6fb2e1fc185f07a4bd3ea6bf74bd0ad
Tom Coleman <tom@thesnail.org>
2015-02-27 16:18:12 +1100

    This won’t work until 1.0.4

--- src/require-react.js
+++ src/require-react.js
@@ -1,1 +1,4 @@
-React = Npm.require("react/addons");
+// should be able to do this, but it doesn't work yet: https://github.com/meteor/meteor/issues/3505
+// React = Npm.require("react/addons");
+
+React = Npm.require("react");
